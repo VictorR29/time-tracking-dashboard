52b735aa81e93a6fb8132c89734968540ee4b502
VictorR29 <victormramonesd@gmail.com>
2022-11-12 16:50:57 -0500

    Iniciando scripts

---
+++ main.js
@@ -0,0 +1,5 @@
+import data from "./data.json" assert {type: "json"};
+
+let dailyBtn = document.querySelector("#daily");
+let weeklyBtn = document.querySelector("#weekly");
+let monthlyBtn = document.querySelector("#monthly");
